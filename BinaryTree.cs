4aec5f62eae947f1daef6a4358c177164bbd6c73
Jim Horton <38473243+jlhorton72@users.noreply.github.com>
2018-11-09 17:57:11 -0600

    first check in

---
+++ BinaryTree.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Name:       James Horton
+/// Date:       11/09/2018
+/// Assignment: Binary Search Tree
+/// </summary>
+
+namespace BinarySearchTreeApplication
+{
+    /// <summary>
+    /// this defines our binary search tree
+    /// </summary>
+    class BinaryTree
+    {
+        /// <summary>
+        /// defines the two elements needed to 
+        /// navigate and establish the tree
+        /// </summary>
+        #region ---Public Class Fields ---
+        public TreeNodes myRoot;
+        public TreeNodes current;   
+        #endregion -- publics --
+
+        /// <summary>
+        ///  no arg constructor that initializes myRoot
+        /// </summary>
+        public BinaryTree()
+        {
+            myRoot = null;
+        } // end of no arg constructor
+
+        /// <summary>
+        /// returns the value of the actual tree root
+        /// </summary>
+        /// <returns></returns>
+        public TreeNodes GetRoot()
+        {
+            return myRoot;
+        } // end of GetRoot 
+
+        #region --- Methods ---
+
+        /// <summary>
+        /// adds a new node to the tree
+        /// or establishes its root
+        /// </summary>
+        /// <param name="addTo"></param>
+        public void AddToTree(char addTo)
+        {
+            TreeNodes nextNode = new TreeNodes();       // create new node instance
+            nextNode.letter = addTo;                    // sets node to new value
+            if (myRoot == null)             // if null value becomes master root
+                myRoot = nextNode;          // if not, the else kicks in to being finding where to hang node
+            else
+            {
+                TreeNodes current = myRoot;
+                TreeNodes parent;
+                while (true)
+                {
+                    parent = current;               // this starts at root first time thru
+                    if (addTo < current.letter)     // checks left side   
+                    {
+                        current = current.left;
+                        if(current == null)         // looking for null
+                        {
+                            parent.left = nextNode;
+                            return;
+                        } // end of if
+                    } // end of if
+                    else
+                    {                               // this check right sides
+                        current = current.right;
+                        if (current == null)        // looking for a null 
+                        {
+                            parent.right = nextNode;
+                            return;
+                        } // end of if
+                    } // end of else
+
+                } // end of while
+            } // end of else
+
+        } // end of AddToTree
+
+        /// <summary>
+        /// this will recursively call to do a walk 
+        /// thru of the tree
+        /// </summary>
+        /// <param name="Root"></param>
+        public void walkThru(TreeNodes Root)
+        {
+            if (Root != null)
+            {
+                walkThru(Root.left);                // recursive call to find the null
+                Console.Write(Root.letter + ", ");  // prints the node value
+                walkThru(Root.right);               // recursive call to find the null
+            } // end of if
+        } // end of walkThru
+
+        /// <summary>
+        /// this will search the tree for a value
+        /// Call is recursive until it search entire tree or
+        /// finds the value
+        /// </summary>
+        /// <param name="subRoot"></param>
+        /// <param name="findMe"></param>
+        /// <returns></returns>
+        public bool Search(TreeNodes subRoot, char findMe)
+        {     
+            if (subRoot == null)
+            {
+                Console.WriteLine("End of tree reached!");
+                return false;
+            } // end of if root null
+            else if (subRoot.letter.Equals(findMe))
+            {
+                Console.WriteLine("This character is in the tree!!");
+                return false;
+            } // end of if
+            else if (findMe < subRoot.letter)
+            {
+                Console.WriteLine("This character is NOT in our tree!");
+                return Search(subRoot.left, findMe);
+            } // end of esle if 
+            else if (findMe > subRoot.letter)
+            {
+                Console.WriteLine("This character is NOT in our tree!");
+                return Search(subRoot.right, findMe);
+            } // end of else if
+
+            return true;
+        } // end of Search
+
+        #endregion -- Methods --
+
+    } // end of class BinaryTree
+} // end of namespace
---
+++ TreeNodes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Name:       James Horton
+/// Date:       11/09/2018
+/// Assignment: Binary Search Tree
+/// </summary>
+
+namespace BinarySearchTreeApplication
+{
+    /// <summary>
+    /// defines the TreeNode
+    /// </summary>
+    class TreeNodes
+    { 
+        public char letter { get; internal set; }
+        public TreeNodes left { get; internal set; }
+        public TreeNodes right { get; internal set; }
+
+        /// <summary>
+        /// defines the individual node
+        /// </summary>
+        class Node
+        {
+            public char value;
+            public Node left;
+            public Node right;
+        } // end of class Node
+
+
+        /// <summary>
+        /// this class defines the Tree and how to add to the tree
+        /// </summary>
+        class Tree
+        {
+            /// <summary>
+            /// this method will insert the new node 
+            /// in the correct place thru recursive calls
+            /// </summary>
+            /// <param name="subRoot"></param>
+            /// <param name="val"></param>
+            /// <returns></returns>
+            public Node insert (Node subRoot, char val)
+            {
+                if (subRoot == null)       // verifies root for original val
+                {
+                    subRoot = new Node();
+                    subRoot.value = val;
+                } // end of if
+                else if (val < subRoot.value)  // checks left side and recurses
+                {
+                    subRoot.left = insert(subRoot.left, val);
+                } // end of else if         // else checks right risde and recurses
+                else
+                {
+                    subRoot.right = insert(subRoot.right, val);
+                } // end of else
+
+                return subRoot;             // returns the node
+            } // end of insert
+
+            /// <summary>
+            /// this method should walk thru the tree
+            /// by using recursive call
+            /// </summary>
+            /// <param name="subroot"></param>
+            public void walkThru(Node subroot)
+            {
+                if (subroot == null)            // null mens empty and it ends
+                {
+                    return;
+                } // end of if
+
+                walkThru(subroot.left);         // checks left side and recalls
+                walkThru(subroot.right);        // checks right side and recalls
+
+            } // end of walkThru
+
+        } // end of class Tree
+
+    } // end of class TreeNodes
+} // end of namspace
